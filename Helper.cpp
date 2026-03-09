ba0eca6ae0b449a1b30758741845a355c4fe60a3
Amit-Sherman <amit112006@gmail.com>
2026-03-10 00:25:31 +0200

    Added expected information gain functionality

--- Helper.cpp
+++ Helper.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <unordered_map>
 #include <cmath>
+#include <algorithm>
 
 #define WORD_LENGTH 5
 
@@ -20,6 +21,7 @@ std::vector<std::string> load_data(std::string filename)
     while (wordsFile >> word) {
         data.push_back(word);
     }
+    wordsFile.close();
     return data;
 }
 
@@ -62,7 +64,7 @@ int translate_to_pattern(std::vector<int> feedback)
     auto result = 0;
     for(int i = 0; i < WORD_LENGTH; i++)
     {
-        result += pow(3, i) * feedback[WORD_LENGTH-i];
+        result += pow(3, i) * feedback[i];
     }
     return result;
 }
@@ -79,4 +81,32 @@ std::vector<std::string> filter_words(const std::vector<std::string>& possible_w
         }
     }
     return filtered_words;
+}
+
+int get_entropy(const std::vector<std::string>& possible_words, const std::string& guess)
+{
+    double entropy = 0;
+    for(int pattern = 0; pattern < 243; pattern++)
+    {
+        auto filtered_words = filter_words(possible_words, guess, pattern);
+        double p = filtered_words.size()/possible_words.size();
+        entropy += p*filtered_words.size();
+    }
+    return entropy;
+}
+
+std::vector<std::pair<std::string, int>> best_words_to_guess(const std::vector<std::string>& possible_words)
+{
+    std::vector<std::pair<std::string, int>> best_words;
+    for(auto word : possible_words)
+    {
+        best_words.push_back({word, get_entropy(possible_words, word)});
+    }
+
+    std::sort(best_words.begin(), best_words.end(),
+        [](const std::pair<std::string,int>& a, const std::pair<std::string,int>& b) {
+            return a.second > b.second;
+        });
+
+    return best_words;
 }
